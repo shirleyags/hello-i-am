6c647e9e88b32800539b9e087c21e1b56512c41f
ShirleyAlice <shirleyags@yahoo.com.br>
2024-08-17 21:47:08 -0300

    Insere componente Footer

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,8 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 import Menu from "./components/Menu"
 import  AboutMe  from "./pages/AboutMe"
 import  Initial  from "./pages/Home"
+import Footer from "./components/Footer"
 
 function AppRoutes() {
   return(
@@ -12,8 +13,9 @@ function AppRoutes() {
         <Route path="/sobremim" element={<AboutMe/>}/>
         <Route path="*" element={<div>Página não encontrada</div>}/>
       </Routes>
+      <Footer></Footer>
     </BrowserRouter>
   )
 }
 
-export default AppRoutes;
+export default AppRoutes
