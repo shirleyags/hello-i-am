4654cb4c705c5bb158678fa1d05a0cde5d75f2db
ShirleyAlice <shirleyags@yahoo.com.br>
2024-08-17 21:40:51 -0300

    Cria componente Footer e estilização

---
+++ src/components/Footer/index.js
@@ -0,0 +1,14 @@
+import "components/Footer/Footer.css"
+import { ReactComponent as TradeMark } from "assets/marca_registrada.svg" 
+
+const Footer = () => {
+  return(
+    <footer className="footer">
+      <TradeMark/>
+      Desenvolvido por Shirley Alice
+    </footer>
+
+  )
+}
+
+export default Footer
